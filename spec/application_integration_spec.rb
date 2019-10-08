cbded6d78ba87a8bd9bd920c7b761833a3717016
Wookeun Song <wookeun.s@gmail.com>
2019-10-08 14:46:40 -0500

    Done.

--- spec/application_integration_spec.rb
+++ spec/application_integration_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper'
  
 describe "GET '/' - Greeting Form" do
+  
   # Code from previous example
   it 'welcomes the user' do
     visit '/'
     expect(page.body).to include("Welcome!")
   end
  
-  # New test
+  # New tests
+
   it 'has a greeting form with a user_name field' do
     visit '/'
  
@@ -25,5 +27,5 @@ describe "GET '/' - Greeting Form" do
       expect(page).to have_text("Hi Avi, nice to meet you!")
     end
   end
-  
+
 end
